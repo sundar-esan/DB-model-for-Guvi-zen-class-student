63f2c8ac89cba33b596bee749846fe2a119325ad
SUNDAR ESAN <84663506+sundar-esan@users.noreply.github.com>
2022-04-27 23:32:53 +0530

    Add files via upload

---
+++ index.sql
@@ -0,0 +1,33 @@
+CREATE TABLE `GuviZenClassStudent` (
+  `Name` Sundaresan,
+  `Email_id` sundaresan17feb,
+  `course` Full_Stack_Development,
+  `Location` Salem
+);
+
+CREATE TABLE `Batch` (
+  `Batch` B30WDTamil,
+  `WeekDayStudent` Yes,
+  `WeekEndStudent` NO,
+  `Language_preference` Tamil
+);
+
+CREATE TABLE `Task` (
+  `JAVA_SCRIPT` completed,
+  `CSS` completed,
+  `HTML` completed,
+  `React_JS` completed,
+  `MYSQL` completed,
+  `MONGODB` completed,
+  `NODE_JS` completed
+);
+
+CREATE TABLE `Mentor` (
+  `JAVA_SCRIPT` SaiMohan,
+  `CSS` SaiMohan,
+  `HTML` SaiMohan,
+  `React_JS` RaghavKumar,
+  `MYSQL` RaghavKumar,
+  `MONGODB` RaghavKumar,
+  `NODE_JS` RaghavKumar
+);
